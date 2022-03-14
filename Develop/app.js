9864cc7e1b9a96e76a09e6a2b2badd1ff4b56b37
786-go <noorullahw@gmail.com>
2022-03-14 16:26:16 -0700

    save button functionality

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,8 @@
 
 currentDay.textContent = moment().format('dddd MMMM Do YYYY');
 
+$(".saveBtn").on("click", function(){
+    var userEntry = $(this).siblings("textarea").val()
+    var id = $(this).siblings("textarea").attr("id")
+    console.log(userEntry,id)
+})
